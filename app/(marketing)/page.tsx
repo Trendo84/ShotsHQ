fe281f310fce2d75eeba222251f29a2af4298bd0
Ivan Sajtovi <ivansajtovi@gmail.com>
2026-05-23 02:33:30 +1000

    refactor: remove extra homepage sections

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,9 +3,6 @@ import { PipelineDiagram } from "@/components/marketing/PipelineDiagram";
 import { FeatureGrid } from "@/components/marketing/FeatureGrid";
 import { Templates } from "@/components/marketing/Templates";
 import { Surfaces } from "@/components/marketing/Surfaces";
-import { Roadmap } from "@/components/marketing/Roadmap";
-import { Comparison } from "@/components/marketing/Comparison";
-import { Faq } from "@/components/marketing/Faq";
 import { CTA } from "@/components/marketing/CTA";
 import { MetricsBand } from "@/components/marketing/MetricsBand";
 import { Reveal } from "@/components/Reveal";
@@ -21,9 +18,6 @@ export default function LandingPage() {
       <Reveal as="div"><Surfaces /></Reveal>
       <Reveal as="div"><Templates compact /></Reveal>
       <Reveal as="div"><FeatureGrid /></Reveal>
-      <Reveal as="div"><Comparison /></Reveal>
-      <Reveal as="div"><Roadmap /></Reveal>
-      <Reveal as="div"><Faq /></Reveal>
       <Reveal as="div"><CTA /></Reveal>
     </>
   );
